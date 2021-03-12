7d2b4c0465abadb5e8980b251105d45c3913b7b0
598465994 <598465994@qq.com>
2021-03-12 03:32:12 +0000

    基础页面

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+
+@section('title', '关于')
+
+@section('content')
+    <h1>这是关于</h1>
+@stop
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+
+@section('title', '帮助')
+
+@section('content')
+    <h1>这是帮助</h1>
+@stop
