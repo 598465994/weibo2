c4a54fa3499b9437261a72a39cfe69ca0522f548
598465994 <598465994@qq.com>
2021-03-12 08:16:23 +0000

    数据库迁移

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,16 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+//HasFactory 是模型工厂相关功能的引用
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+//Authenticatable 是授权相关功能的引用
 use Illuminate\Foundation\Auth\User as Authenticatable;
+//Notifiable 是消息通知相关功能引用
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
+    //table属性来定义要进行数据交互的数据库表名称
+    protected $table = 'users';
+
     /**
      * The attributes that are mass assignable.
+     * fillable：过滤用户提交的字段，只用包含在该属性中的字段才能被正常更新
      *
      * @var array
      */
@@ -24,6 +31,7 @@ class User extends Authenticatable
 
     /**
      * The attributes that should be hidden for arrays.
+     * 当我们需要对用户密码或其它敏感信息在用户实例通过数组或 JSON 显示时进行隐藏
      *
      * @var array
      */
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -8,29 +8,33 @@ class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
+     * 当我们运行迁移时，up方法会被调用
      *
      * @return void
      */
     public function up()
     {
+        //Schema类的create方法来创建表。create 方法会接收两个参数：一个是数据表的名称，另一个则是接收 $table（Blueprint 实例）的闭包。这里$table就是值得时users表
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->id(); //id() 是 bigIncrements() 的封装，此方法创建了一个 bigint unsigned 类型的自增长 id。
+            $table->string('name'); //由 string 方法创建了一个 name 字段， 用于保存用户名称。
+            $table->string('email')->unique(); //由 string 方法创建了一个 email 字段，且在最后指定该字段的值为唯一值，用于保存用户邮箱。
+            $table->timestamp('email_verified_at')->nullable(); //Email 验证时间，空的话意味着用户还未验证邮箱。nullable：为空
+            $table->string('password', 60); //由 string 方法创建了一个 password 字段，且在 string 方法中指定保存的值最大长度为 60，用于保存用户密码。
+            $table->rememberToken(); //由 rememberToken 方法为用户创建一个 remember_token 字段，用于保存『记住我』的相关信息。
+            $table->timestamps(); //由 timestamps 方法创建了一个 created_at 和一个 updated_at 字段，分别用于保存用户的创建时间和更新时间。
         });
     }
 
     /**
      * Reverse the migrations.
+     * 当我们回滚迁移时，down方法会被调用
      *
      * @return void
      */
     public function down()
     {
+        //dropIfExists时删除表
         Schema::dropIfExists('users');
     }
 }
