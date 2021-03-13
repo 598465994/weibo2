e9bb2470dd5b96ea89034c59d0265131055729dd
598465994 <598465994@qq.com>
2021-03-13 07:11:37 +0000

    登录成功后导航逻辑

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Auth;
 
 class UsersController extends Controller
 {
@@ -34,11 +35,15 @@ class UsersController extends Controller
             'password' => 'required|confirmed|min:6'
         ]);
 
+        // create方法是插入成功返回一个新的模型实例
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => bcrypt($request->password)
         ]);
+
+        Auth::login($user);
+
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
     }
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -2,8 +2,36 @@
     <div class="container ">
       <a class="navbar-brand" href="{{ route('home') }}">Weibo App</a>
       <ul class="navbar-nav justify-content-end">
-        <li class="nav-item"><a class="nav-link" href="{{ route('help') }}">帮助</a></li>
-        <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
+        {{-- 用户已经登录 --}}
+        @if (Auth::check())
+          <li class="nav-item"><a class="nav-link" href="#">用户列表</a></li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                {{-- 登录用户的名称 --}}
+              {{ Auth::user()->name }}
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                {{-- 跳转用户的个人信息页面 --}}
+              <a class="dropdown-item" href="{{ route('users.show', Auth::user()) }}">个人中心</a>
+              <a class="dropdown-item" href="#">编辑资料</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" id="logout" href="#">
+                  {{-- 用户退出表单提交 --}}
+                <form action="{{ route('logout') }}" method="POST">
+                    {{-- 防止CSRF攻击 --}}
+                  {{ csrf_field() }}
+                    {{-- 浏览器模拟delete请求 --}}
+                  {{ method_field('DELETE') }}
+                  <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+                </form>
+              </a>
+            </div>
+          </li>
+        {{-- 用户没有登录 --}}
+        @else
+          <li class="nav-item"><a class="nav-link" href="{{ route('help') }}">帮助</a></li>
+          <li class="nav-item" ><a class="nav-link" href="{{ route('login') }}">登录</a></li>
+        @endif
       </ul>
     </div>
   </nav>
