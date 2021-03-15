78a01d7e2106b76216adad89657b84631778f9e2
598465994 <598465994@qq.com>
2021-03-15 02:24:46 +0000

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -47,4 +47,39 @@ class UsersController extends Controller
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
     }
+
+    /**
+     * 编辑用户资料页面
+     * 这里的User $user 隐性路由模型绑定get传递的参数查询出对应的用户id信息
+     */
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    /**
+     * 用户信息修改
+     * $user 第一个为自动解析用户 id 对应的用户实例对象
+     * $request 第二个则为更新用户表单的输入数据
+     */
+    public function update(Uesr $user, Request $request)
+    {
+        // nullable提交空白也能通过
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+
+        $user->update($data);
+
+        session()->flash('success', '个人资料更新成功！');
+
+        return redirect()->route('users.show', $user->id);
+    }
 }
---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.default')
+@section('title', '更新个人资料')
+
+@section('content')
+<div class="offset-md-2 col-md-8">
+  <div class="card ">
+    <div class="card-header">
+      <h5>更新个人资料</h5>
+    </div>
+      <div class="card-body">
+        {{-- 引入错误提示 --}}
+        @include('shared._errors')
+
+        {{-- 用户头像 --}}
+        <div class="gravatar_edit">
+          <a href="http://gravatar.com/emails" target="_blank">
+            <img src="{{ $user->gravatar('200') }}" alt="{{ $user->name }}" class="gravatar"/>
+          </a>
+        </div>
+
+        <form method="POST" action="{{ route('users.update', $user->id )}}">
+            {{-- 伪造 PATCH 请求 --}}
+            {{ method_field('PATCH') }}
+            {{-- 防止CSRF攻击 --}}
+            {{ csrf_field() }}
+            {{-- 用户名称 --}}
+            <div class="form-group">
+              <label for="name">名称：</label>
+              <input type="text" name="name" class="form-control" value="{{ $user->name }}">
+            </div>
+            {{-- 用户邮箱 --}}
+            <div class="form-group">
+              <label for="email">邮箱：</label>
+              <input type="text" name="email" class="form-control" value="{{ $user->email }}" disabled>
+            </div>
+            {{-- 用户密码 --}}
+            <div class="form-group">
+              <label for="password">密码：</label>
+              <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+            </div>
+            {{-- 确认密码 --}}
+            <div class="form-group">
+              <label for="password_confirmation">确认密码：</label>
+              <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
+            </div>
+
+            <button type="submit" class="btn btn-primary">更新</button>
+        </form>
+    </div>
+  </div>
+</div>
+@stop
